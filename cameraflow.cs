9561de3019bd2a45abf2486a765951bbfa0efb2c
Mustapha sraidi <mustapha.sraidi@gmail.com>
2022-03-26 23:22:21 +0100

    Add files via upload

---
+++ cameraflow.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraflow : MonoBehaviour {
+
+    public Transform PlayerTransform;
+
+    private Vector3 _cameraOffset;
+
+    [Range(0.01f, 1.0f)]
+    public float SmoothFactor = 1;
+
+    public bool LookAtPlayer = false;
+
+	// Use this for initialization
+	void Start () {
+        _cameraOffset = transform.position - PlayerTransform.position;	
+	}
+	
+	// LateUpdate is called after Update
+	void Update () {
+        Vector3 newPos = PlayerTransform.position + _cameraOffset;
+
+        transform.position = Vector3.Slerp(transform.position, newPos, SmoothFactor);
+
+        
+
+	} 
+
+}
